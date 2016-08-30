9a6b79dec2380d0ec85fe7b79086af88985476a7
Juri Pebriant <junkuwieazy@yahoo.com>
2016-08-30 10:42:13 +0700

    New Tab Login Admin
    New tab after click login admin button

--- View/DaftarKonsultasi.php
+++ View/DaftarKonsultasi.php
@@ -63,7 +63,7 @@
           <li class="dropdown">
             <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Admin <span class="caret"></span></a>
             <ul class="dropdown-menu">
-              <li><a href="?url=login">Login Admin</a></li>
+              <li><a target="_blank" href="?url=login">Login Admin</a></li>
             </ul>
           </li>
         </ul>
